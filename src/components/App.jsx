3d51927a1d6a73b295a7b5e7e3a9e3c46f2c1a3e
Bertware <bert.marcelis@outlook.com>
2018-07-10 10:10:15 +0200

    Added initial sidebar #8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,13 @@ import Header from "./Header.jsx";
 require('../css/leaflet.1.3.1.css');
 require('../css/materialize.min.css');
 require('../css/material-icons.css');
-
+require('../css/App.css');
 require('../js/materialize.min.js');
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className={"application"}>
                 <Header className={"application__header"}/>
                 <Row className={"application__content"}>
                     <Sidebar/>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,9 +9,10 @@ export default class OpenStreetMap extends Component {
     render() {
         return (
             <Col m={3} s={12} className="sidebar">
-                <Card className='white darken-1' title='Search results'
+                <Card className='sidebar__card white darken-1' title='Search results'
                       actions={[<a href='#'>This is a link</a>]}>
 
+                    <SearchResult/>
                     <SearchResult/>
                     <SearchResult/>
                     <SearchResult/>
