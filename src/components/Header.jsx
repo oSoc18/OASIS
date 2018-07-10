106f86b7429967f105d2d9a41c0cef6387669ce8
Lisa <hennecart.lisa@gmail.com>
2018-07-10 14:36:17 +0200

    #24 edit haeder search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,6 +20,15 @@ export default class Header extends Component {
                                 <i className="material-icons">close</i>
                         </div>
                     </form>
+                    <div className="input-field col s12">
+                        <select multiple>
+                            <option value="" disabled selected>Choose your option</option>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                            <option value="3">Option 3</option>
+                        </select>
+                        <label>Materialize Multiple Select</label>
+                    </div>
                 </div>
 
             </nav>
