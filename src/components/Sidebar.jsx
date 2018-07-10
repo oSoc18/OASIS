3dab92e0890a09fb564be4f5ecf4300e8584a009
Brian Dendauw <brian.dendauw@student.howest.be>
2018-07-10 14:49:39 +0200

    Function to make dynamic buildings sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,17 +5,33 @@ import {Card, Col} from "react-materialize";
 require('../css/Sidebar.css');
 
 export default class Sidebar extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    createSearchResults = () => {
+        let result = [];
+        {this.props.buildings.map((building) => {
+                result.push(<SearchResult title={building.title} src={building.src} about={building.about}  />);
+        })};
+        return  result;
+    }
+
     render() {
         return (
             <Col m={3} s={12} className="sidebar">
                 <Card className='sidebar__card white darken-1' title='Search results'
                       actions={[<a href='#'>This is a link</a>]}>
 
-                    <SearchResult/>
-                    <SearchResult title='Other Building' src='http://placekitten.com/200/300' about='Some random information available'/>
-
+                    {this.createSearchResults()}
                 </Card>
             </Col>
         )
     }
-}               
+}
+
+Sidebar.defaultProps = {
+    buildings: [
+        {title:"building name", src:"http://placekitten.com/200/300", about:"info"}
+    ]
+}
