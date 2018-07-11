3dbd85d12715936931116a0e0e542763397541b1
Bertware <bert.marcelis@outlook.com>
2018-07-11 19:29:37 +0200

    Use the boilerplate code created by create-react-app #48

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,15 @@ export default class Header extends Component {
                 <div className="nav-wrapper row">
 
                     <a href="#" className="brand">
-                        <img className="brand__icon" src="/src/images/logo-oasis.svg" alt="OASIS"></img>
+                        <img className="brand__icon" src={require('../images/logo-oasis.svg')} alt="OASIS"></img>
                         <h1 className="brand__name">OASIS</h1>
                     </a>
                     <form className="search__bar">
                         <div className="input-field">
                             <input id="search" type="search" required></input>
-                                <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
+                                <label className="label-icon" htmlFor="search">
+                                    <i className="material-icons">search</i>
+                                </label>
                                 <i className="material-icons">close</i>
                         </div>
                     </form>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet'
+import * as L from "leaflet";
 
 require('../css/Map.css');
 
@@ -10,47 +11,26 @@ export default class OpenStreetMap extends Component {
         zoom: 11,
     }
 
-    constructor(props){
-        super(props);
-    }
-
-    /**
-     * show marker showing one array one at the time
-     * @param markerIcon this in informotion on the marker icon
-     * @returns {Array} this return the position of the building
-     */
-     showMarkers = (markerIcon) => {
-        let buldingPosition = [];
-         {this.props.buildings.map((building) => {
-             buldingPosition.push(<Marker className="pointer"  position={[building.location.long, building.location.lat]}
-                                          icon={markerIcon} onMouseOver={this.test}>
-                 <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup></Marker>);
-            //return <Marker className="pointer"  position={[building.location.lat, building.location.long]} icon={myIcon}/>
-        })}
-        return buldingPosition;
-    };
-
-test = () =>{
-    alert('test');
-}
-
     render() {
-        var markerIcon = L.icon({
-            iconUrl: "/src/images/map-marker-icon.png",
+        const position = [this.state.lat, this.state.lng]
+        var myIcon = L.icon({
+            iconUrl: require('../images/map-marker-icon.png'),
             iconSize: [38, 38],
             iconAnchor: [19, 38],
             popupAnchor: [-3 -76]
         });
 
         return (
-            <Map center={[51.05389,3.705]} zoom={this.state.zoom}>
+            <Map center={position} zoom={this.state.zoom}>
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <p >i am here</p>
-                {this.showMarkers(markerIcon)}
-
+                <Marker className="pointer" position={position} icon={myIcon}>
+                    <Popup>
+                        A pretty CSS3 popup. <br/> Easily customizable.
+                    </Popup>
+                </Marker>
             </Map>
         )
     }
