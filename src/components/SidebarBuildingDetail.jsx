536dfe4eb5f120ecdfcadacdbd9456316a72fd1d
Brian Dendauw <brian.dendauw@student.howest.be>
2018-07-11 14:15:35 +0200

    Switch states by clicking on more details

--- src/components/SidebarBuildingDetail.jsx
+++ src/components/SidebarBuildingDetail.jsx
@@ -3,11 +3,21 @@ import React, {Component} from 'react'
 require('../css/SidebarBuildingDetail.css');
 
 export default class SidebarBuildingDetail extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    handleClick(e){
+        e.preventDefault();
+        this.props.onClick();
+    }
+    
     render() {
         return (
             <div>
                 <h1>TODO: BuildingDetail.jsx</h1>
+                <a href='#' onClick={this.handleClick}>Return to results</a>
             </div>
         )
     }
