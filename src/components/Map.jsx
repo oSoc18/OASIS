c172d9bd2e6110ef110cc0413ccf9dc234082c62
Lisa <hennecart.lisa@gmail.com>
2018-07-11 11:17:05 +0200

    Fix position of the marker #36 in map.jsx change the first parameter of the aconAncor to 1/2 of the icon size  and the second parameter to the iconsize value and change in the CSS map the magin in negative

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,22 +5,29 @@ require('../css/Map.css');
 
 export default class OpenStreetMap extends Component {
     state = {
-        lat: 51.05389,
+        lat: 49.05389,
         lng: 3.705,
         zoom: 11,
     }
 
     render() {
         const position = [this.state.lat, this.state.lng]
+        var myIcon = L.icon({
+            iconUrl: "/src/images/map-marker-icon.png",
+            iconSize: [38, 38],
+            iconAnchor: [19, 38],
+            popupAnchor: [-3 -76]
+        });
+
         return (
             <Map center={position} zoom={this.state.zoom}>
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker className="pointer" position={position} icon={myIcon}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        A pretty CSS3 popup. <br/> Easily customizable.
                     </Popup>
                 </Marker>
             </Map>
