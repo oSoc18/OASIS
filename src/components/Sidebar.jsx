61d56be0eb5349afb7a4e4d64444a8b416ae1a4d
DendauwBrian <brian.dendauw@student.howest.be>
2018-07-11 13:53:55 +0200

    Made states in Sidebar (#45)
    * Made states in Sidebar
    * Created preperation functions and renamed BuildingDetail classname
    * Created const declaration for states

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,60 @@
 import React, {Component} from 'react'
 import SearchResult from "./SidebarSearchResult.jsx";
 import {Card, Col} from "react-materialize";
+import BuildingDetail from './SidebarBuildingDetail.jsx';
 
 require('../css/Sidebar.css');
 
+const STATE_RESULTS = 'results';
+const STATE_DETAILS = 'details';
 
 export default class Sidebar extends React.Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            currentView: STATE_RESULTS
+        }
+    }
+
+    onClick = () => {
+        let newState = this.state.currentView === STATE_RESULTS ? STATE_DETAILS : STATE_RESULTS;
+        this.setState = {
+            currentView: newState
+        }
     }
 
     createSearchResults = () => {
         let result = [];
         {this.props.buildings.map((building) => {
-                result.push(<SearchResult title={building.title} src={building.src} about={building.about}  />);
+                result.push(<SearchResult buildings={building}  />);
         })};
         return  result;
+    }
+
+    createDetails = () => {
+        let details = [];
+        details.push(<BuildingDetail />);
+        return details;
+    }    
+
+    show = () => {
+        if(this.state.currentView === STATE_RESULTS){
+            return this.createSearchResults();
+        }else if(this.state.currentView === STATE_DETAILS){
+            return this.createDetails();
+        }else{
+            // Tampered state
+        }
     }
 
     render() {
         return (
             <Col m={3} s={12} className="sidebar">
                 <Card className='sidebar__card white darken-1' title='Search results' actions={[<a href='#'>This is a link</a>]}>
-                    {this.createSearchResults()}
+                    {this.show()}
                 </Card>
             </Col>
         )
     }
-}
-
-Sidebar.defaultProps = {
-    buildings: [
-        {title:"building name", src:"http://placekitten.com/200/300", about:"info"}
-    ]
 }
