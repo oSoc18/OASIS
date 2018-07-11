166b8eace9d1f0ff50dab8cfcfe4dbfcbc48e3e7
Lisa <hennecart.lisa@gmail.com>
2018-07-11 15:07:53 +0200

    #33 showing dummy locations on the map locations info are on index.js building array

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,9 +10,26 @@ export default class OpenStreetMap extends Component {
         zoom: 11,
     }
 
+    constructor(props){
+        super(props);
+    }
+
+    /**
+     * show marker showing one array one at the time
+     * @param markerIcon this in informotion on the marker icon
+     * @returns {Array} this return the position of the building
+     */
+     markers = markerIcon => {
+        let buldingPosition = [];
+         {this.props.buildings.map((building) => {
+            buldingPosition.push(<Marker className="pointer"  position={[building.location.long, building.location.lat]} icon={markerIcon}/>);
+            //return <Marker className="pointer"  position={[building.location.lat, building.location.long]} icon={myIcon}/>
+        })}
+        return buldingPosition;
+    };
+
     render() {
-        const position = [this.state.lat, this.state.lng]
-        var myIcon = L.icon({
+        var markerIcon = L.icon({
             iconUrl: "/src/images/map-marker-icon.png",
             iconSize: [38, 38],
             iconAnchor: [19, 38],
@@ -20,16 +37,12 @@ export default class OpenStreetMap extends Component {
         });
 
         return (
-            <Map center={position} zoom={this.state.zoom}>
+            <Map center={[51.05389,3.705]} zoom={this.state.zoom}>
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker className="pointer" position={position} icon={myIcon}>
-                    <Popup>
-                        A pretty CSS3 popup. <br/> Easily customizable.
-                    </Popup>
-                </Marker>
+                {this.markers(markerIcon)}
             </Map>
         )
     }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './components/App.jsx';
 /** This is just for rendering, edit App.jsx instead! **/
 
 const buildings = [
-    {title:"BeCentral", src:"http://placekitten.com/200/300", about:"Information about becentral", location: {long: 51, lat:51}},
-    {title:"City Hall", src:"http://placekitten.com/200/200", about:"Awesome city hall place", location: {long: 31, lat:31}},
-    {title:"Public Service", src:"http://placekitten.com/200/300", about:"Some public service building", location: {long: 51, lat:41}},
-    {title:"Building Name", src:"http://placekitten.com/200/200", about:"Info", location: {long: 40, lat:51}}
+    {title:"BeCentral", src:"http://placekitten.com/200/300", about:"Information about becentral", location: {long: 51.05389, lat:3.705}},
+    {title:"City Hall", src:"http://placekitten.com/200/200", about:"Awesome city hall place", location: {long: 51, lat:4}},
+    {title:"Public Service", src:"http://placekitten.com/200/300", about:"Some public service building", location: {long: 51, lat:2}},
+    {title:"Building Name", src:"http://placekitten.com/200/200", about:"Info", location: {long: 50, lat:5}}
 ];
 
 ReactDOM.render(<App buildingArr={buildings}/>, document.getElementById('root'));
