5025328d880cf8b06bc2ee37c11401740980b867
Bertware <bert.marcelis@outlook.com>
2018-07-12 10:37:49 +0200

    Reverted Map.jsx which was lost in #51

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet'
-import * as L from "leaflet";
 
 require('../css/Map.css');
 
@@ -11,26 +10,47 @@ export default class OpenStreetMap extends Component {
         zoom: 11,
     }
 
+    constructor(props){
+        super(props);
+    }
+
+    /**
+     * show marker showing one array one at the time
+     * @param markerIcon this in informotion on the marker icon
+     * @returns {Array} this return the position of the building
+     */
+     showMarkers = (markerIcon) => {
+        let buldingPosition = [];
+         {this.props.buildings.map((building) => {
+             buldingPosition.push(<Marker className="pointer"  position={[building.location.long, building.location.lat]}
+                                          icon={markerIcon} onMouseOver={this.test}>
+                 <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup></Marker>);
+            //return <Marker className="pointer"  position={[building.location.lat, building.location.long]} icon={myIcon}/>
+        })}
+        return buldingPosition;
+    };
+
+test = () =>{
+    alert('test');
+}
+
     render() {
-        const position = [this.state.lat, this.state.lng]
-        var myIcon = L.icon({
-            iconUrl: require('../images/map-marker-icon.png'),
+        var markerIcon = L.icon({
+            iconUrl: "/src/images/map-marker-icon.png",
             iconSize: [38, 38],
             iconAnchor: [19, 38],
             popupAnchor: [-3 -76]
         });
 
         return (
-            <Map center={position} zoom={this.state.zoom}>
+            <Map center={[51.05389,3.705]} zoom={this.state.zoom}>
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker className="pointer" position={position} icon={myIcon}>
-                    <Popup>
-                        A pretty CSS3 popup. <br/> Easily customizable.
-                    </Popup>
-                </Marker>
+                <p >i am here</p>
+                {this.showMarkers(markerIcon)}
+
             </Map>
         )
     }
